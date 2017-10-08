13a0fe0b53a3dac794398551a6398c5c8d4d646b
Dustin Schimel <dustin.schimel@gmail.com>
2017-10-07 21:15:32 -0600

    Finished all the loops

--- src/loops/controller/LoopsController.java
+++ src/loops/controller/LoopsController.java
@@ -1,10 +1,8 @@
 package loops.controller;
 
 import java.util.List;
-
-import monster.model.MarshmallowMonster;
-
 import java.util.ArrayList;
+
 
 public class LoopsController
 {
@@ -59,14 +57,35 @@ public class LoopsController
 		
 		System.out.println("Your remaining plants will be put into a group.");
 		
-		String plantsArray[] = new String[10];
+		System.out.println("");
 		
-		for (String : plantsArray)
+		String[] plantsArray = {"Plant #1", "Plant #2", "Plant #3", "Plant #4", "Plant #5", "Plant #6", 
+								"Plant #7", "Plant #8", "Plant #9", "Plant #10", };
+		
+		for (String plant : plantsArray)
 		{
-			
+			System.out.println(plant);
 		}
 		
+		System.out.println("");
 		
+		int[] numbersArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 		
+		for (int number : numbersArray)
+		{
+			System.out.println(number);
+		}
+		
+		System.out.println("");
+		
+		for (int loop = 0; loop < 10; loop++)
+		{
+			System.out.println("My favorite number is " + numbersArray[loop]);
+			
+			for (int loop2 = 0; loop2 < 2; loop2++)
+			{
+				System.out.println("Wait no, my favorite number is actually " + numbersArray[loop] + loop2);
+			}
+		}
 	}
 }
